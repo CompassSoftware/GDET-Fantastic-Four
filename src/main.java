4fed3bc55fe654908c7ac01c0dd3b57ce1c1c529
Aidan O'Brien <obrienam@appstate.edu>
2018-11-08 13:06:49 -0500

    Changed file path of log.txt for it to run properly in Eclipse

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 import java.io.*;
 public class main{
     public static void main(String[] args) throws Exception{
-        File file = new File("log.txt");
+        File file = new File("src/log.txt");
 
         BufferedReader br = new BufferedReader(new FileReader(file));
 
