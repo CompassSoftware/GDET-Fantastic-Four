3655ef4e6ad53f0e35a0803adedd4740d0ee9df5
Aidan O'Brien <obrienam@appstate.edu>
2018-11-09 19:54:12 -0500

    Updated to run curl commands within java.

--- src/main.java
+++ src/main.java
@@ -1,19 +1,44 @@
 import java.io.*;
+import java.lang.ProcessBuilder;
 public class main{
     public static void main(String[] args) throws Exception{
         File file = new File("src/log.txt");
-
-        BufferedReader br = new BufferedReader(new FileReader(file));
-
+        
+        
+        
+        ProcessBuilder pb = new ProcessBuilder(
+            "curl",
+            "-i",
+            "-u",
+            "obrienam:MsTsYnR7826!",
+            "https://api.github.com/repos/JaysGitLab/cs5666-javatrix-the-fantastic-four/commits" 
+            /**"|",
+            "(grep",
+            "-E",
+            "'name|date|message'",
+            "|",
+            "grep",
+            "-vwE",
+            "'payload|\"name\":",
+            "\"GitHub\"')"**/
+            );
+        pb.redirectErrorStream(true);
+        Process p = pb.start();
+        InputStream is = p.getInputStream();
+        BufferedInputStream bi = new BufferedInputStream(is);
         String st;
+        BufferedReader br = new BufferedReader(new InputStreamReader(bi));
+        
         
         LinkedList cleanup = new LinkedList();
             
         while ((st = br.readLine()) != null){
             cleanup.insert(st.trim());
             //else if(st.substring(1, 8).equals("message")){
+            //System.out.println(st);
         }
 
+        
         String[][] info = new String[3][cleanup.getSize() / 3];
 
         Node curr = cleanup.getHead();
@@ -30,6 +55,8 @@ public class main{
             System.out.println("");
         }
         //cleanup.print();
-
+        
+         
+        br.close();
     }
 }
